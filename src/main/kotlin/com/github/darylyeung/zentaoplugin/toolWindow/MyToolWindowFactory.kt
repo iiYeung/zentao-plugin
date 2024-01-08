4643ca340bb1d307395f9dd71e7713119d245431
Yeung <i.yeung@foxmail.com>
2024-01-08 22:33:13 +0800

    fix:cancel token verification

--- src/main/kotlin/com/github/darylyeung/zentaoplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/darylyeung/zentaoplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,7 +1,5 @@
 package com.github.darylyeung.zentaoplugin.toolWindow
 
-import com.github.darylyeung.zentaoplugin.common.Constant
-import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
@@ -15,14 +13,6 @@ class MyToolWindowFactory : ToolWindowFactory {
     }
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        if (PropertiesComponent.getInstance().isValueSet(Constant.TOKEN_KEY.value)) {
-            thisLogger().info("token is set")
-            //  login success, show product list
-            val listPanel = ProductListPanel().createListPanel(toolWindow)
-            val content = ContentFactory.getInstance().createContent(listPanel, null, false)
-            toolWindow.contentManager.addContent(content)
-            return
-        }
         //  create login panel
         val loginPanel = LoginPanel().createPanel(toolWindow)
         //  add login panel to tool window
