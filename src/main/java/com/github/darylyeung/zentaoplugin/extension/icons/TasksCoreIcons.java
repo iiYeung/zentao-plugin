0ca586f38910b419fd4b1c6d66201f1dab574104
Yeung <i.yeung@foxmail.com>
2024-01-10 23:57:29 +0800

    feat:extension task plugin

---
+++ src/main/java/com/github/darylyeung/zentaoplugin/extension/icons/TasksCoreIcons.java
@@ -0,0 +1,22 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.github.darylyeung.zentaoplugin.extension.icons;
+
+import com.intellij.ui.IconManager;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * NOTE THIS FILE IS AUTO-GENERATED
+ * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
+ */
+public final class TasksCoreIcons {
+    private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
+        return IconManager.getInstance().loadRasterizedIcon(path, TasksCoreIcons.class.getClassLoader(), cacheKey, flags);
+    }
+
+    /**
+     * 16x16
+     */
+    public static final @NotNull Icon ZenTao = load("icons/zentao.svg", 842371061, 0);
+}
